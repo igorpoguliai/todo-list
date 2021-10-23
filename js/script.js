285e3b25e75265aaf08f272484d353df4aea7c12
Igor <tit0k.gigi@gmail.com>
2021-10-24 00:39:56 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-alert(1)
+// const todoInput = document.getElementById('input-value');
+// const todoList = document.getElementById('list');
+// const notes = [];
+
+// todoInput.addEventListener('submit', event => {
+//   event.preventDefault();
+//   let newNote = {
+//     note: todoInput.value,
+//     checked: false,
+//     id: Math(random),
+//   }
+//   todoInput.value = '';
+//   notes.push(newNote);
+//   addNotes();
+// })
+
+// function addNotes() {
+//   let message = '';
+
+//   notes.forEach((item, index) => {
+//     message += `
+//     <li>${index + 1}. ${item.note};</li>
+//     `;
+//     todoList.innerHTML = message;
+//   })
+// }
+alert(hy)
